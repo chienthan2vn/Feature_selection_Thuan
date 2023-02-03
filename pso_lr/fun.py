2012fd2696a5d2250657ad9a369c059cf040dd99
Kanna <thuanluong19102001@266432.onmicrosoft.com>
2023-02-04 00:48:04 +0700

    update pso-lr

---
+++ pso_lr/fun.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+def fun(x, y, fea, opts, alpha):
+    if np.count_nonzero(fea) == 0: X_subset = x
+    else: X_subset = x[:,fea==1]
+    lr = LogisticRegression()
+    lr.fit(X_subset, y)
+    p = (lr.predict(X_subset) == y).mean()
+    j = (alpha * (1.0 - p) + (1.0 - alpha) * (1 - (X_subset.shape[1] / x.shape[1])))
+    
+    return j
---
+++ pso_lr/pso.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a = [1,2,3,4,5]
+
+
+print([1 for i in a if i > 2])
