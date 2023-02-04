9f4b4fadfae70895dd53e1517da8ba468a11f4da
Kanna <thuanluong19102001@266432.onmicrosoft.com>
2023-02-05 00:20:18 +0700

    update pso

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup, find_packages
 
-from pso_lr import __version__
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='pso_lr',
-    version=__version__,
+    name='Feature_selection_Thuan',
+    version='0.0.1',
     description='Feature Selection using PSO_LR.',
     url='https://github.com/chienthan2vn/Feature_selection_Thuan',
     author='Thuan Luong',
     author_email='thuanluong19102001@gmail.com',
-    packages=find_packages(),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    project_urls = {
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = find_packages(where="src"),
+    python_requires = ">=3.6"
 )
