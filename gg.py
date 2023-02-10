3c2a11ed7477d580ade84aee9be16a454a55f059
Kanna <thuanluong19102001@266432.onmicrosoft.com>
2023-02-10 14:39:01 +0700

    update tlbo

---
+++ gg.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+a = np.array([[1,2,2],[2,3,4]])
+print(np.size(a, axis=1))
--- src/tlbo_lr/tlbo.py
+++ src/tlbo_lr/tlbo.py
@@ -1,58 +1,117 @@
-"""
-This code performs a feature selection using the TLBO algorithm to minimize a given fitness function. 
-The user can define the problem parameters,
-such as the number of iterations, population size, and the search space boundaries. 
-The code returns the final population and their corresponding fitness values.
-"""
-import random
 import numpy as np
+from numpy.random import rand
+import sys
+sys.path.append("./src")
+from cso_lr.fun import fun
+import math
 
-# Define a fitness function
-def fitness_function(x):
-    return x**2 + x + 1
 
-# TLBO function
-def TLBO(fitness_function, dimension, n_iterations, population_size, a, b, p, q):
-    # Initialize population
-    population = np.random.uniform(a, b, (population_size, dimension))
-    fitness = np.zeros(population_size)
-    for i in range(population_size):
-        fitness[i] = fitness_function(population[i])
+def init_position(lb, ub, N, dim):
+    X = np.zeros([N, dim], dtype='float')
+    for i in range(N):
+        for d in range(dim):
+            X[i,d] = lb[0,d] + (ub[0,d] - lb[0,d]) * rand()        
     
-    # Start TLBO loop
-    for i in range(n_iterations):
-        for j in range(population_size):
-            # Select three random individuals
-            indices = [k for k in range(population_size) if k != j]
-            random_indices = random.sample(indices, 3)
-            x1, x2, x3 = population[random_indices[0]], population[random_indices[1]], population[random_indices[2]]
-            
-            # Generate a new candidate
-            mutant_vector = x1 + p * (x2 - x3)
-            cross_over_point = np.random.randint(0, dimension)
-            trial_vector = np.copy(population[j])
-            trial_vector[:cross_over_point] = mutant_vector[:cross_over_point]
-            
-            # Calculate its fitness
-            trial_fitness = fitness_function(trial_vector)
-            
-            # Update population
-            if trial_fitness < fitness[j]:
-                population[j] = trial_vector
-                fitness[j] = trial_fitness
+    return X
+
+
+def binary_conversion(X, thres, N, dim):
+    Xbin = np.zeros([N, dim], dtype='int')
+    for i in range(N):
+        for d in range(dim):
+            if X[i,d] > thres:
+                Xbin[i,d] = 1
             else:
-                if random.random() < q:
-                    population[j] = trial_vector
-    return population, fitness
+                Xbin[i,d] = 0
+    
+    return Xbin
 
-# Define problem parameters
-dimension = 2
-n_iterations = 1000
-population_size = 20
-a, b = -10, 10
-p, q = 0.5, 0.5
 
-# Run TLBO
-population, fitness = TLBO(fitness_function, dimension, n_iterations, population_size, a, b, p, q)
-print("Final population:", population)
-print("Final fitness:", fitness)
+def boundary(x, lb, ub):
+    if x < lb:
+        x = lb
+    if x > ub:
+        x = ub
+    
+    return x
+
+def jfs(xtrain, ytrain, opts):
+    # Parameter
+    ub = 1
+    lb = 0
+    thres = 0
+    r = rand(1)
+
+    N           = opts['N']
+    max_iter    = opts['max_iter']
+    if 'Tf' in opts:
+        Tf = opts['Tf']
+    if 'r' in opts:
+        r = opts['r']
+
+    # Dimension
+    dim = np.size(xtrain, 1)
+    if np.size(lb) == 1:
+        ub = ub * np.ones([1, dim], dtype='float')
+        lb = lb * np.ones([1, dim], dtype='float')
+
+    # Initialize position
+    X = init_position(lb, ub, N, dim)
+
+    # Binary conversion
+    Xbin  = binary_conversion(X, thres, N, dim)
+
+    # Fitness at first iteration (Ham muc tieu)
+    fit   = np.zeros([N, 1], dtype='float')
+    Xgb   = np.zeros([1, dim], dtype='float')
+    fitG  = float('inf')
+
+    for i in range(N):
+        fit[i,0] = fun(xtrain, ytrain, Xbin[i,:], opts)
+        if fit[i,0] < fitG:
+            Xgb[0,:] = X[i,:]
+            fitG     = fit[i,0]
+
+    # Pre
+    curve = np.zeros([1, max_iter], dtype='float') 
+    t     = 0
+
+    curve[0,t] = fitG.copy()
+    print("Generation:", t + 1)
+    print("Best (FA):", curve[0,t])
+    t += 1
+
+    while t < max_iter:
+        for i in range(N):
+            Xmean = np.sum(X, axis=0)/np.size(X, axis=0)
+            Tf = round(1+rand(1))
+            X = X + r*(Xgb - Tf*Xmean)
+
+            temp = np.zeros([1, dim], dtype='float')
+            temp[0,:] = X[i,:]  
+            Xbin = binary_conversion(temp, thres, 1, dim)
+
+            # fitness
+            fit[i,0]  = fun(xtrain, ytrain, Xbin[0,:], opts)
+            
+            # best update        
+            if fit[i,0] < fitG:
+                Xgb[0,:] = X[i,:]
+                fitG     = fit[i,0]
+                
+        # Store result
+        curve[0,t] = fitG.copy()
+        print("Generation:", t + 1)
+        print("Best (FA):", curve[0,t])
+        t += 1
+
+    # Best feature subset
+    Gbin       = binary_conversion(Xgb, thres, 1, dim) 
+    Gbin       = Gbin.reshape(dim)
+    pos        = np.asarray(range(0, dim))    
+    sel_index  = pos[Gbin == 1]
+    num_feat   = len(sel_index)
+    # Create dictionary
+    tlbo_data = {'sf': sel_index, 'c': curve, 'nf': num_feat}
+    
+    return tlbo_data    
